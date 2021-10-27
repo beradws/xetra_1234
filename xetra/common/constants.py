6b2377fc57eb77e10ffab8b33e71f4d2571806b1
beradws <vanbakel.brad@gmail.com>
2021-10-27 16:38:08 +1300

    Entry point + pipenv implemented

--- xetra/common/constants.py
+++ xetra/common/constants.py
@@ -18,5 +18,5 @@ class MetaProcessFormat(Enum):
     META_DATE_FORMAT = '%Y-%m-%d'
     META_PROCESS_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
     META_SOURCE_DATE_COL = 'source_date'
-    META_PROCESS_COL = 'dateime_of_processing'
+    META_PROCESS_COL = 'datetime_of_processing'
     META_FILE_FORMAT = 'csv'
